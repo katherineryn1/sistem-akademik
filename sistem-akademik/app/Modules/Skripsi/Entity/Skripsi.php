1be166eb707774d72ce9759388aef73fabb250f6
cynthiacc512 <cynthiacc512@gmail.com>
2022-05-09 18:26:01 +0700

    [BE] skripsi, masih error

--- sistem-akademik/app/Modules/Skripsi/Entity/Skripsi.php
+++ sistem-akademik/app/Modules/Skripsi/Entity/Skripsi.php
@@ -22,89 +22,89 @@ class Skripsi{
 	}
 
 
-	/**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
+// 	/**
+//      * @return int
+//      */
+//     public function getId(): int
+//     {
+//         return $this->id;
+//     }
 
-    /**
-     * @param int $id
-     */
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
-
-
-    /**
-     * @return string
-     */
-    public function getJudul(): string
-    {
-        return $this->judul;
-    }
-
-    /**
-     * @param string $judul
-     */
-    public function setJudul(string $judul): void
-    {
-        $this->judul = $judul;
-    }
-
-	/**
-     * @return string
-     */
-    public function getBatasAkhir(): string
-    {
-        return $this->batasAkhir;
-    }
-
-    /**
-     * @param string $batasakhir
-     */
-    public function setBatasAkhir(string $batasAkhir): void
-    {
-        $this->batasAkhir = $batasAkhir;
-    }
+//     /**
+//      * @param int $id
+//      */
+//     public function setId(int $id): void
+//     {
+//         $this->id = $id;
+//     }
 
 
-	/**
-     * @return string
-     */
-    public function getFile(): string
-    {
-        return $this->file;
-    }
+//     /**
+//      * @return string
+//      */
+//     public function getJudul(): string
+//     {
+//         return $this->judul;
+//     }
 
-    /**
-     * @param string $file
-     */
-    public function setFile(string $file): void
-    {
-        $this->file = $file;
-    }
+//     /**
+//      * @param string $judul
+//      */
+//     public function setJudul(string $judul): void
+//     {
+//         $this->judul = $judul;
+//     }
 
-	/**
-     * @return int
-     */
-    public function getMilestone(): int
-    {
-        return $this->milestone;
-    }
+// 	/**
+//      * @return string
+//      */
+//     public function getBatasAkhir(): string
+//     {
+//         return $this->batasAkhir;
+//     }
 
-    /**
-     * @param int $milestone
-     */
-    public function setMilestone(int $milestone): void
-    {
-        $this->milestone = $milestone;
-    }
+//     /**
+//      * @param string $batasakhir
+//      */
+//     public function setBatasAkhir(string $batasAkhir): void
+//     {
+//         $this->batasAkhir = $batasAkhir;
+//     }
 
-}
+
+// 	/**
+//      * @return string
+//      */
+//     public function getFile(): string
+//     {
+//         return $this->file;
+//     }
+
+//     /**
+//      * @param string $file
+//      */
+//     public function setFile(string $file): void
+//     {
+//         $this->file = $file;
+//     }
+
+// 	/**
+//      * @return int
+//      */
+//     public function getMilestone(): int
+//     {
+//         return $this->milestone;
+//     }
+
+//     /**
+//      * @param int $milestone
+//      */
+//     public function setMilestone(int $milestone): void
+//     {
+//         $this->milestone = $milestone;
+//     }
+
+// }
 
 
 
